d9b2b9f7cf0cc81e4fc840d7e7b610b5496a86ed
hellowesleyr <44436509+hellowesleyr@users.noreply.github.com>
2022-06-23 11:07:39 +0100

    Add Images and File structure

---
+++ memory-game/app.js
@@ -0,0 +1,38 @@
+const cardArray = [
+    {
+        name: 'ronnie',
+        img: ''
+    },
+
+    {
+        name: 'catgirl',
+        img: 'catgirl.png'
+    }
+
+    {
+        name:,
+        img:
+    },
+
+    {
+        name: ,
+        img:
+    },
+    {
+        name: ,
+        img:
+    },
+    {
+        name: ,
+        img:
+    },
+    {
+        name: ,
+        img:
+    },
+    {
+        name: ,
+        img:
+    },
+    
+]
